1cb346a06b17a6eba74c6781c35b099e05097aab
geoff.b.stewart@gmail.com <geoff.b.stewart@gmail.com@f5a1cab6-20b6-64af-0367-a26788a01379>
2010-10-05 01:35:16 +0000

    again, try to seed a new project

---
+++ trunk/GuideEnricher/GuideEnricher/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: geoff
+ * Date: 28/09/2010
+ * Time: 9:12 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.ServiceProcess;
+using System.Text;
+
+namespace GuideEnricher
+{
+	static class Program
+	{
+		/// <summary>
+		/// This method starts the service.
+		/// </summary>
+		static void Main()
+		{
+			// To run more than one service you have to add them here
+			ServiceBase.Run(new ServiceBase[] { new GuideEnricher() });
+		}
+	}
+}
