798774414579fed89a38d7954499672935b72fb6
malingo@dodgethis.de <malingo@dodgethis.de@f5a1cab6-20b6-64af-0367-a26788a01379>
2010-10-12 21:53:39 +0000

    Testapplication to be able to debug in the IDE

---
+++ GuideEnricher/GuideEnricher/TestApplication.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using ForTheRecord.ServiceAgents;
+using System.ServiceModel;
+using ForTheRecord.Entities;
+
+namespace GuideEnricher
+{
+    static class TestApplication
+    {
+        static void Main()
+        {
+
+            Thread.Sleep(2000);
+
+            ServerSettings serverSettings = new ServerSettings();
+            serverSettings.ServerName = "ganymed";
+            serverSettings.Transport = ServiceTransport.NetTcp;
+            serverSettings.Port = 49942;
+
+            ServiceChannelFactories.Initialize(serverSettings, false);
+
+            ServiceHost sh = ForTheRecordListener.CreateServiceHost(Config.getProperty("serviceUrl"));
+
+            sh.Open();
+
+            using (ForTheRecordServiceAgent agent = new ForTheRecordServiceAgent())
+            {
+                ForTheRecordEventGroup eventGroupsToListenTo = ForTheRecordEventGroup.ScheduleEvents;
+                agent.EnsureEventListener(eventGroupsToListenTo, Config.getProperty("serviceUrl"), Constants.EventListenerApiVersion);
+                Console.Read();
+            }
+
+        }
+    }
+}
