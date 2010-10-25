b1baffbc39746b47235f969e039c032fd38508a2
geoff.b.stewart@gmail.com <geoff.b.stewart@gmail.com@f5a1cab6-20b6-64af-0367-a26788a01379>
2010-10-25 01:03:04 +0000

    finally get the threading to work so that the guide enriching happens in a different thread than the event handling

--- GuideEnricher/GuideEnricher/GuideEnricher.cs
+++ GuideEnricher/GuideEnricher/GuideEnricher.cs
@@ -7,6 +7,7 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -24,11 +25,20 @@ using ForTheRecord.Client.Common;
 
 namespace GuideEnricher
 {
+   /**
+    * Class that defines the service start/stop behaviours
+    */
    public class GuideEnricher : ServiceBase
    {
       public const string MyServiceName = "GuideEnricher";
 
-      ForTheRecordServiceAgent agent;
+      private string MODULE="GuideEnricher";
+
+      public static EventWaitHandle waitHandle = new AutoResetEvent(false);
+      
+      private Thread worker = null;
+      
+      private bool workerLoop = true;
       
       public GuideEnricher()
       {
@@ -38,6 +48,10 @@ namespace GuideEnricher
       private void InitializeComponent()
       {
          this.ServiceName = MyServiceName;
+         
+         worker = new Thread(enrichGuideData);
+
+         
       }
       
       /// <summary>
@@ -54,48 +68,41 @@ namespace GuideEnricher
       /// </summary>
       protected override void OnStart(string[] args)
       {
+//         Thread.Sleep(10000);
          
-         Thread.Sleep(2000);
-
          try
          {
-             ServerSettings serverSettings = new ServerSettings();
-             serverSettings.ServerName = "localhost";
-             serverSettings.Transport = ServiceTransport.NetTcp;
-             serverSettings.Port = 49942;
+            ServerSettings serverSettings = new ServerSettings();
+            serverSettings.ServerName = "localhost";
+            serverSettings.Transport = ServiceTransport.NetTcp;
+            serverSettings.Port = 49942;
 
-             if (!ServiceChannelFactories.Initialize(serverSettings, false))
-             {
-                 Logger.Info("Unable to connect to ForTheRecordService, check your settings.");
-                 
-             }
-             else
-             {
-                 Logger.Info("Connected to ForTheRecordService");
-             
+            if (!ServiceChannelFactories.Initialize(serverSettings, false))
+            {
+               Logger.Info("Unable to connect to ForTheRecordService, check your settings.");
+               
+            }
+            else
+            {
+               Logger.Info("Connected to ForTheRecordService");
+               
+               ServiceHost sh = ForTheRecordListener.CreateServiceHost(Config.getProperty("serviceUrl"));
 
-                 //         ftrl = new ForTheRecordListener();
-
-                 ServiceHost sh = ForTheRecordListener.CreateServiceHost(Config.getProperty("serviceUrl"));
-
-                 try
-                 {
-                     sh.Open();
-                 }
-                 catch (System.ServiceProcess.TimeoutException ex)
-                 {
-                     Logger.Error("Timeout on creating the ServiceHost", ex.Message);
-                 }
-                 catch (Exception ex)
-                 {
-                     Logger.Error("Error on creating ServiceHost:{0}{1}", Environment.NewLine, ex.Message);
-                 }
-
-                 agent = new ForTheRecordServiceAgent();
-                 ForTheRecordEventGroup eventGroupsToListenTo = ForTheRecordEventGroup.ScheduleEvents;
-                 agent.EnsureEventListener(eventGroupsToListenTo, Config.getProperty("serviceUrl"), Constants.EventListenerApiVersion);
-
-             }
+               try
+               {
+                  sh.Open();
+                  
+               }
+               catch (System.ServiceProcess.TimeoutException ex)
+               {
+                  Logger.Error("Timeout on creating the ServiceHost", ex.Message);
+               }
+               catch (Exception ex)
+               {
+                  Logger.Error("Error on creating ServiceHost:{0}{1}", Environment.NewLine, ex.Message);
+               }
+               
+            }
 
          }
          catch
@@ -103,6 +110,8 @@ namespace GuideEnricher
 
          }
          
+         // start worker thread
+         worker.Start();
          
       }
       
@@ -111,12 +120,94 @@ namespace GuideEnricher
       /// </summary>
       protected override void OnStop()
       {
-          if (agent != null)
-          {
-              agent.RemoveEventListener(Config.getProperty("serviceUrl"));
-              agent.Dispose();
-          }
+         Logger.Info("Ending the worker thread...");
+         workerLoop = false;
+         waitHandle.Set();
+         
       }
       
+      /**
+       * Main worker thread method
+       * 
+       * Loop that will find all upcoming recordings, enrich data, update in database
+       * if the episode information changed
+       */
+      
+      public void enrichGuideData() {
+         IDataEnricher de = DataEnricherFactory.Instance.getGuideDataEnricher();
+         
+         while (workerLoop) {
+            
+            Logger.Info("Thread waiting for events...");
+            
+            // wait until the listener thread signals us to update the guide data
+            waitHandle.WaitOne();
+            if (!workerLoop) {
+               break;
+            }
+            
+            UpcomingRecording[] upcomingRecs = null;
+            ArrayList enrichedPrograms = new ArrayList();
+            
+            using (TvControlServiceAgent tcsa = new TvControlServiceAgent()) {
+               upcomingRecs = tcsa.GetAllUpcomingRecordings(UpcomingRecordingsFilter.Recordings,false);
+            }
+            
+            Logger.Info("{0}: Starting the process to enrich guide data.  Processing {1} upcoming shows",
+                        MODULE, Convert.ToString(upcomingRecs.Length));
+            
+            for (int i = 0; i < upcomingRecs.Length; i++) {
+               UpcomingProgram upProg = upcomingRecs[i].Program;
+
+               if (upProg.GuideProgramId != null)
+               {
+                  GuideProgram prog = null;
+                  using (TvGuideServiceAgent tgsa = new TvGuideServiceAgent()) {
+                     prog = tgsa.GetProgramById((Guid)upProg.GuideProgramId);
+                  }
+
+                  // enrich program
+                  try {
+                     string oldEpisodeNumber = prog.EpisodeNumberDisplay;
+                     IProgramSummary updatedProgram = de.enrichProgram(prog);
+                     
+                     if (!prog.EpisodeNumberDisplay.Equals(oldEpisodeNumber)) {
+                        // program was actually enriched
+                        prog.EpisodeNumberDisplay = updatedProgram.EpisodeNumberDisplay;
+                        enrichedPrograms.Add(prog);
+                     }
+                  } catch (Exception ex) {
+                     Logger.Error("Error enriching program: {0}",ex.Message);
+                  }
+
+
+               }
+               
+            }
+            // if any programs were enriched, do the import ot update in teh db
+            if (enrichedPrograms.Count > 0) {
+               using (TvGuideServiceAgent tgsa = new TvGuideServiceAgent()) {
+                  Logger.Info("{0}: About to commit enriched guide data. {1} entries were enriched",
+                              MODULE, Convert.ToString(enrichedPrograms.Count));
+                  GuideProgram[] progArray = new GuideProgram[enrichedPrograms.Count];
+                  for (int i = 0; i < enrichedPrograms.Count; i++) {
+                     progArray[i] = (GuideProgram)enrichedPrograms[i];
+                  }
+                  tgsa.ImportPrograms(progArray, GuideSource.XmlTv);
+               }
+            } else {
+               Logger.Info("{0}: No programs were enriched.",
+                           MODULE);
+            }
+            
+            Logger.Info("{0}: Done enriching guide data", MODULE);
+            
+
+            
+         }
+      }
+
+      
    }
+ 
 }
--- GuideEnricher/GuideEnricher/Program.cs
+++ GuideEnricher/GuideEnricher/Program.cs
@@ -7,21 +7,34 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ServiceProcess;
 using System.Text;
+using System.Threading;
+
+using ForTheRecord.ServiceAgents;
+using ForTheRecord.ServiceContracts;
+using ForTheRecord.Entities;
+using ForTheRecord.Client.Common;
 
 namespace GuideEnricher
 {
-	static class Program
-	{
-		/// <summary>
-		/// This method starts the service.
-		/// </summary>
-		static void Main()
-		{
-			// To run more than one service you have to add them here
-			ServiceBase.Run(new ServiceBase[] { new GuideEnricher() });
-		}
-	}
+   static class Program
+   {
+      
+//      private static string MODULE="windowService";
+      
+      /// <summary>
+      /// This method starts the service.
+      /// </summary>
+      static void Main()
+      {
+         // To run more than one service you have to add them here
+         ServiceBase.Run(new ServiceBase[] { new GuideEnricher() });
+      }
+      
+
+      
+   }
 }
