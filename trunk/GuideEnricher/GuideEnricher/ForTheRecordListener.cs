799436d81fccc13e5f07ae7e8ba9095f04fc450a
geoff.b.stewart@gmail.com <geoff.b.stewart@gmail.com@f5a1cab6-20b6-64af-0367-a26788a01379>
2010-10-25 01:03:04 +0000

    finally get the threading to work so that the guide enriching happens in a different thread than the event handling

--- trunk/GuideEnricher/GuideEnricher/ForTheRecordListener.cs
+++ trunk/GuideEnricher/GuideEnricher/ForTheRecordListener.cs
@@ -7,14 +7,15 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.Collections;
 using System.ServiceModel;
 using System.ServiceModel.Description;
-using System.Collections;
+using System.Threading;
 
-using ForTheRecord.ServiceContracts.Events;
 using ForTheRecord.Client.Common;
 using ForTheRecord.Entities;
 using ForTheRecord.ServiceAgents;
+using ForTheRecord.ServiceContracts.Events;
 
 namespace GuideEnricher
 {
@@ -22,27 +23,22 @@ namespace GuideEnricher
    /// Description of ForTheRecordListener.
    /// </summary>
    [ServiceBehavior(
-      ConcurrencyMode = ConcurrencyMode.Single,
+      ConcurrencyMode = ConcurrencyMode.Multiple,
       InstanceContextMode = InstanceContextMode.Single)]
    public class ForTheRecordListener : EventsListenerServiceBase
    {
+      private GuideEnricher windowsService = null;
       private static string MODULE = "GuideEnricherListener";
-      protected IDataEnricher de;
       
       public ForTheRecordListener()
       {
-         DataEnricherFactory def = DataEnricherFactory.Instance;
-         de = def.getGuideDataEnricher();
-                       
-         
-         
-         
+         windowsService = new GuideEnricher();
       }
       
       public static ServiceHost CreateServiceHost(string eventsServiceBaseUrl)
       {
          ServiceHost sh = CreateServiceHost(typeof(ForTheRecordListener), eventsServiceBaseUrl, typeof(IGuideEventsListener),
-                                  typeof(IScheduleEventsListener));
+                                            typeof(IScheduleEventsListener));
          
          return sh;
       }
@@ -50,84 +46,33 @@ namespace GuideEnricher
       public override void NewGuideData()
       {
          Logger.Verbose("{0}: Handle NewGuideData event",MODULE);
-         enrichGuideData();
+         signalOtherThread();
       }
       
       public override void UpcomingRecordingsChanged()
       {
          Logger.Verbose("{0}: Handle UpcomingRecordingsChanged event",MODULE);
-         enrichGuideData();
+         signalOtherThread();
       }
 
       public override void UpcomingSuggestionsChanged()
       {
          Logger.Verbose("{0}: Handle UpcomingSuggestionsChanged event",MODULE);
-         enrichGuideData();
+         signalOtherThread();
       }
 
       public override void ScheduleChanged(Guid scheduleId)
       {
          Logger.Verbose("{0}: Handle ScheduleChanged event",MODULE);
-         enrichGuideData();
+         signalOtherThread();
       }
       
-      protected void enrichGuideData() {
-         UpcomingRecording[] upcomingRecs = null;
-         ArrayList enrichedPrograms = new ArrayList();
-         
-         using (TvControlServiceAgent tcsa = new TvControlServiceAgent()) {
-            upcomingRecs = tcsa.GetAllUpcomingRecordings(UpcomingRecordingsFilter.Recordings,false);
-         }
-         
-         Logger.Info("{0}: Starting the process to enrich guide data.  Processing {1} upcoming shows",
-                     MODULE, Convert.ToString(upcomingRecs.Length));
-         
-         for (int i = 0; i < upcomingRecs.Length; i++) {
-            UpcomingProgram upProg = upcomingRecs[i].Program;
-
-            if (upProg.GuideProgramId != null)
-            {
-               GuideProgram prog = null;
-               using (TvGuideServiceAgent tgsa = new TvGuideServiceAgent()) {
-                  prog = tgsa.GetProgramById((Guid)upProg.GuideProgramId);
-               }
-
-                // enrich program
-                try {
-                   string oldEpisodeNumber = prog.EpisodeNumberDisplay;
-                   IProgramSummary updatedProgram = de.enrichProgram(prog);
-   
-                   if (!prog.EpisodeNumberDisplay.Equals(oldEpisodeNumber)) {
-                     // program was actually enriched
-                      prog.EpisodeNumberDisplay = updatedProgram.EpisodeNumberDisplay;
-                      enrichedPrograms.Add(prog);
-                   }
-                } catch (Exception ex) {
-                   Logger.Error("Error enriching program: {0}",ex.Message);
-                }
-
-
-            }       
-              
-         }
-         // if any programs were enriched, do the import ot update in teh db
-         if (enrichedPrograms.Count > 0) {
-            using (TvGuideServiceAgent tgsa = new TvGuideServiceAgent()) {
-               Logger.Info("{0}: About to commit enriched guide data. {1} entries were enriched",
-                           MODULE, Convert.ToString(enrichedPrograms.Count));
-               GuideProgram[] progArray = new GuideProgram[enrichedPrograms.Count];
-               for (int i = 0; i < enrichedPrograms.Count; i++) {
-                  progArray[i] = (GuideProgram)enrichedPrograms[i];
-               }
-               tgsa.ImportPrograms(progArray, GuideSource.XmlTv);
-            }
-         } else {
-             Logger.Info("{0}: No programs were enriched.",
-                        MODULE);
-         }
-         
-         Logger.Info("{0}: Done enriching guide data", MODULE);
+      private void signalOtherThread() {
+         Logger.Info("{0}: signal worker thread",MODULE);
+         GuideEnricher.waitHandle.Set();
       }
-   
+      
+
+      
    }
 }
