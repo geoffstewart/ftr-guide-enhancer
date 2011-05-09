5d19c48666897eac6c51d27b4ad980939ba8ea07
stephane.jeffrey@gmail.com <stephane.jeffrey@gmail.com@f5a1cab6-20b6-64af-0367-a26788a01379>
2011-05-09 19:25:31 +0000

    Refactored TVDBLibEnrichTest to use Setup method

--- trunk/GuideEnricher/GuideEnricher.Tests/TVDBLibEnrichTest.cs
+++ trunk/GuideEnricher/GuideEnricher.Tests/TVDBLibEnrichTest.cs
@@ -5,27 +5,34 @@
     using NUnit.Framework;
 
     [TestFixture]
-    public class TVDBLibTest
+    public class TVDBLibEnrichTest
     {
+        private TvDbDataEnricher enricher;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.enricher = new TvDbDataEnricher();
+        }
+
         [Test]
         public void TestEnricherWithProgramNameAsSubTitle()
         {
             var program = new GuideProgram();
             program.Title = "The Big Bang Theory";
             program.SubTitle = "The Zazzy Substitution";
-            var enricher = new TvDbDataEnricher();
-            enricher.enrichProgram(program);
+            this.enricher.enrichProgram(program);
             Assert.AreEqual("S04E03", program.EpisodeNumberDisplay);
         }
 
         [Test]
         public void TestEnricherWithAbsoluteEpisodeNumber()
         {
+            // I know this one fails, need some examples of actual guide entries...
             var program = new GuideProgram();
             program.Title = "The Big Bang Theory";
             program.SubTitle = "66";
-            var enricher = new TvDbDataEnricher();
-            enricher.enrichProgram(program);
+            this.enricher.enrichProgram(program);
             Assert.AreEqual("S04E03", program.EpisodeNumberDisplay);
         }
     }
