f54f4c0df75aab4ff4b8754579b30c2aaf20ab11
malingo@dodgethis.de <malingo@dodgethis.de@f5a1cab6-20b6-64af-0367-a26788a01379>
2014-01-04 11:00:12 +0000

    Added three more MatchMethods: One finds the Season- and Episodenumber in the description, one tries the first sentence in the description as the EpisodeTitle, one tries to find the EpisodeTitle in single quotes in the description.

---
+++ GuideEnricher/GuideEnricher/EpisodeMatchMethods/InQuotesInDescriptionMatchMethod.cs
@@ -0,0 +1,37 @@
+﻿namespace GuideEnricher.EpisodeMatchMethods
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using GuideEnricher.Model;
+
+    using TvdbLib.Data;
+
+    public class InQuotesInDescriptionMatchMethod : MatchMethodBase
+    {
+        protected System.Text.RegularExpressions.Regex quotedSentence = new System.Text.RegularExpressions.Regex(@"(?<=').*?(?=')");
+
+        public override string MethodName
+        {
+            get
+            {
+                return "Inside Single Quotes in Description";
+            }
+        }
+
+        public override bool Match(GuideEnricherProgram enrichedGuideProgram, List<TvdbEpisode> episodes)
+        {
+            this.MatchAttempts++;
+            var match = quotedSentence.Match(enrichedGuideProgram.Description);
+            if (match != null && !string.IsNullOrEmpty(match.Value))
+            {
+                var matchedEpisode = episodes.FirstOrDefault(x => x.EpisodeName == match.Value);
+                if (matchedEpisode != null)
+                {
+                    return this.Matched(enrichedGuideProgram, matchedEpisode);
+                }
+            }
+            return false;
+        }
+    }
+}
